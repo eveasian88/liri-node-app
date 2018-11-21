6f173a681909f32cc6e3041867ae4e95dcd002e4
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-11-20 20:10:40 -0800

    wip

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# liri-node-app
+## liri-node-app
+
+LIRI is a Node.js application that takes in a command along with a search phrase and returns information based on the command entered. The commands it recognizes are: - concert-this - spotify-this-song - movie-this - do-what-it-says
+
+Command Format: 'command' 'search phrase'
+
+• concert-this 'band name'
+
+This takes the band name entered and runs it through the bands in town API and returns concert information for that particular band. The following is logged in the terminal:
+Venue Name
+Location of Venue
+Date of the Concert
+
+• spotify-this-song 'song title'
+
+This takes the song title entered and runs it through the spotify API. The following is logged to the terminal:
+Artist
+Song Title
+Spotify Preview Link
+Album
+concert-this 'band name'
+
+• movie-this 'movie title'
+
+This takes the movie title entered and runs it through the OMDB API and returns:
+Movie Title
+Year Released
+IMDB Rating
+Rotten Tomatoes Rating
+Countries where the movie was produced
+Language
+Plot
+Actor
--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+var fs = require("fs");
 var keys = require("./keys")
+var request = require("request");
 
 var actions = {
     CONCERT_THIS: "concert-this",
@@ -7,53 +9,118 @@ var actions = {
     MOVIE_THIS: "movie-this",
     DO_WHAT_IT_SAYS: "do-what-it-says",
 }
+// step 1 liri needs to process the command and call the appropriate functions
+var command = process.argv[2];
+var parameter = process.argv[3];
 
-var command = process.argv[2]
-
-switch (command){
-    case actions.CONCERT_THIS: 
+// step 2 process function
+switch (command) {
+    // 2a concert-this needs to take artist name and request data from API and give output
+    case actions.CONCERT_THIS:
         // console.log("concert.function");
         concertThis();
         break;
+    // 2b spotify-this-song needs to take song name and request data from API and give output
     case actions.SPOTIFY_THIS_SONG:
         // console.log("spotify.function");
         spotifyThis();
         break;
+    // 2c movie-this needs to take movie name and request data from API and give output
     case actions.MOVIE_THIS:
         // console.log("music.function");
         movieThis();
         break;
+    // 2d do-what-it-says needs to read information from text file and request data from somewhere and give output - command in text file
     case actions.DO_WHAT_IT_SAYS:
         // console.log("doWhatItSays.function");
         doWhatItSays();
         break;
+    // default when user doesn't fill in all necessary information
     default:
         console.log("invalid input");
 }
 
-function concertThis(){
-    console.log("concert.function");
+function concertThis(parameter) {
+    // console.log("concert.function");
+    if (action === "concert-this") {
+        var artistName = "";
+        for (var i = 3; i < process.argv.length; i++) {
+            artistName += process.argv[i];
+        }
+        console.log(artistName);
+    } else {
+        artistName = parameter;
+    }
 }
-function spotifyThis(){
-    console.log(spotify.function);
+
+var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
+
+
+request(queryUrl, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+
+        var JS = JSON.parse(body);
+        for (i = 0; i < JS.length; i++) {
+            var dTime = JS[i].datetime;
+            var month = dTime.substring(5, 7);
+            var year = dTime.substring(0, 4);
+            var day = dTime.substring(8, 10);
+            var dateForm = month + "/" + day + "/" + year
+
+            logIt("\n-------------------------\n");
+            logIt("Date: " + dateForm);
+            logIt("Name: " + JS[i].venue.name);
+            logIt("City: " + JS[i].venue.city);
+            if (JS[i].venue.region !== "") {
+                logIt("Country: " + JS[i].venue.region);
+            }
+            logIt("Country: " + JS[i].venue.country);
+            logIt("\n-------------------------\n");
+        }
+    }
+});
+
+
+function spotifyThis(parameter) {
+    // console.log(spotify.function);
+    var searchTrack;
+    // if no song is provided then your program will default to "The Sign" by Ace of Base.
+    if (parameter === undefined) {
+        searchTrack = '"The Sign" by Ace of Base"';
+    } else {
+        searchTrack = parameter;
+    }
+
+    spotify.search({
+        type: "track",
+        query: searchTrack
+    }, function (error, data) {
+        if (error) {
+            logIt("Error occurred: " + error);
+            return;
+        } else {
+            logIt("\n-------------------------\n");
+            logIt("Date: " + dateForm);
+            logIt("Name: " + JS[i].venue.name);
+            logIt("City: " + JS[i].venue.city);
+            if (JS[i].venue.region !== "") {
+            logIt("Country: " + JS[i].venue.region);
+            }
+            logIt("Country: " + JS[i].venue.country);
+            logIt("\n-------------------------\n");
+        }
+    });
+};
+
+
+function movieThis() {
+    console.log("movie.function");
+
 }
-function movieThis(){
-    console.log(movie.function);
+function doWhatItSays() {
+    console.log("doWhatItSays.function");
 }
-function doWhatItSays(){
-    console.log(doWhatItSays.function);
-}
-
-
-
-// step 1 liri needs to process the command and call the appropriate functions
-
-// step 2 process function
-
-// 2a concert-this needs to take artist name and request data from API and give output
-// 2b spotify-this-song needs to take song name and request data from API and give output
-// 2c movie-this needs to take movie name and request data from API and give output
-// 2d do-what-it-says needs to read information from text file and request data from somewhere and give output - command in text file
 
 
 
@@ -77,8 +144,5 @@ function doWhatItSays(){
 
 // * The album that the song is from
 
-// * If no song is provided then your program will default to "The Sign" by Ace of Base.
 
-// * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
 
-// * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
